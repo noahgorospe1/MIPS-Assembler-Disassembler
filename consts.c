6ce518bade928cd2ea661bfac3d77f4c74344642
anadik913 <arn00010@mix.wvu.edu>
2026-04-14 16:39:23 -0400

    Documentation is updated for consts.c

--- consts.c
+++ consts.c
@@ -69,140 +69,129 @@ const struct instruction_definition instruction_definitions[] = {
 
 
     {
-        "ANDI",        // MIPS instruction name
-        I_TYPE,     	// Instruction Type [Changed from r_TYPE]
-        "001100",
-        NULL,
-        { RT, RS, IMM, EMPTY } // Rearranged the order of the registers
+        "ANDI",                 // MIPS instruction name
+        I_TYPE,     	        // Instruction Type [Changed from R_TYPE]
+        "001100",               // Opcode
+        NULL,                   // Funct Code
+        { RT, RS, IMM, EMPTY }  // Registers [order rearranged]
     },
 
 
     {
-        "BEQ",      // MIPS instruction name
-        I_TYPE,   // Instruction Type [Changed from r_TYPE]
-
-
-        "000100", // Opcode [Changed from 000000]
-        NULL,     // Removed the function field
-        { RS, RT, IMM, EMPTY }
+        "BEQ",                  // MIPS instruction name
+        I_TYPE,                 // Instruction Type [Changed from r_TYPE]
+        "000100",               // Opcode [Changed from 000000]
+        NULL,                   // Removed the function field
+        { RS, RT, IMM, EMPTY }  // Registers
     },
 
 
     {
-        "DIV",  // MIPS instruction name
-        R_TYPE,
-        "000000",
-        "011010",
-        { RS, RT, EMPTY, EMPTY } // Added RT register
+        "DIV",                  // MIPS instruction name
+        R_TYPE,                 // Instruction Type
+        "000000",               // Opcode
+        "011010",               // Funct Code
+        { RS, RT, EMPTY, EMPTY } // registers [Added RT register]
     },
 
 
     {
-        "LUI", // MIPS instruction name
-        I_TYPE,
-        "001111",
-        NULL,
-        { RT, IMM, EMPTY, EMPTY } // removed RS register and updated RT register
+        "LUI",                    // MIPS instruction name
+        I_TYPE,                   // Instruction Type
+        "001111",                 // Opcode
+        NULL,                     // Funct Code
+        { RT, IMM, EMPTY, EMPTY } // Registers [Removed RS register and updated RT register]
     },
 
 
     {
-        "LW",   // MIPS instruction name
-        I_TYPE,     // Instruction Type [Changed from R_TYPE]
-        "100011",
-        NULL,
-        { RT, RS, IMM, EMPTY } // rearranged the order of the registers
+        "LW",                  // MIPS instruction name
+        I_TYPE,                // Instruction Type [Changed from R_TYPE]
+        "100011",              // Opcode
+        NULL,                  // Funct Code
+        { RT, RS, IMM, EMPTY } // Registers [Rearranged the order of registers]
     },
 
 
     {
-        "MFHI", // MIPS instruction name
-        R_TYPE,
-        "000000",
-        "010000",   // Updated function code [FROM 010010]
+        "MFHI",                     // MIPS instruction name
+        R_TYPE,                     // Instruction Type
+        "000000",                   // Opcode
+        "010000",                   // Funct Code [Changed from 010010]
         { RD, EMPTY, EMPTY, EMPTY } // Removed RS register and updated to RD register
     },
 
 
     {
-        "MFLO",     // MIPS instruction name
-        R_TYPE,
-        "000000",
-        "010010", // Updated function code [from 010000]
+        "MFLO",                     // MIPS instruction name
+        R_TYPE,                     // Instruction Type
+        "000000",                   // Opcode
+        "010010",                   // Funct code [Changed from 010000]
         { RD, EMPTY, EMPTY, EMPTY } // Removed RS register and updated to RD register
     },
 
 
     {
-        "OR", // MIPS instruction name
-        R_TYPE,
-        "000000", // Updated the opcode [from 001101
-        "100101", // Updated function code [from 000000]
-        { RD, RS, RT, EMPTY }
+        "OR",                   // MIPS instruction name
+        R_TYPE,                 // Instruction type
+        "000000",               // Updated the opcode [from 001101
+        "100101",               // Updated function code [from 000000]
+        { RD, RS, RT, EMPTY }   // Registers
     },
 
 
     {
-        "SLT",  // MIPS instruction name
-        R_TYPE,
-        "000000",
-        "101010", // Funct code [Changed from 101011]
-        { RD, RS, RT, EMPTY } // Rearranged the order of the registers
+        "SLT",                // MIPS instruction name
+        R_TYPE,               // Instruction type
+        "000000",             // Opcode
+        "101010",             // Funct code [Changed from 101011]
+        { RD, RS, RT, EMPTY } // Registers [Rearranged the order of registers]
     },
 
 
     {
-        "SUB",  // MIPS instruction name
-        R_TYPE,
-        "000000", // Opcode [Changed from 100010]
-        "100010", // Funct Code [Changed from 000000]
-        { RD, RS, RT, EMPTY } // Added RD register
+        "SUB",                  // MIPS instruction name
+        R_TYPE,                 // Instruction Type
+        "000000",               // Opcode [Changed from 100010]
+        "100010",               // Funct Code [Changed from 000000]
+        { RD, RS, RT, EMPTY }   // Added RD register
     },
 
 
     {
-        "SW",   // MIPS instruction name
-        I_TYPE,
-        "101001", // Opcode [Changed from 100011]
-        NULL,
-        { RT, RS, IMM, EMPTY } // Rearranged Register values
+        "SW",                   // MIPS instruction name
+        I_TYPE,                 // Instruction Type
+        "101001",               // Opcode [Changed from 100011]
+        NULL,                   // Funct Code
+        { RT, RS, IMM, EMPTY }  // Registers [Rearranged values]
     },
    
     // TODO: Implement MULT, ORI, and SLTI instructions once bugs are fixed
 
-
     // The following instructions have been newly implemented
     {
-        "MULT", 	// MIPS instruction name
-        R_TYPE,	// Instruction Type
-        "000000", // Opcode
-        "011000",	// Funct code
-        { RS, RT, EMPTY, EMPTY } // Registers
+        "MULT", 	            // MIPS instruction name
+        R_TYPE,	                // Instruction Type
+        "000000",               // Opcode
+        "011000",	            // Funct code
+        { RS, RT, EMPTY, EMPTY }// Registers
     },
 
 
     {
-        "ORI",  // MIPS instruction name
-        I_TYPE,
-        "001101",
-        NULL,
-        { RT, RS, IMM, EMPTY }
+        "ORI",                  // MIPS instruction name
+        I_TYPE,                 // Instruction Type
+        "001101",               // Opcode
+        NULL,                   // Funct code
+        { RT, RS, IMM, EMPTY }  // Registers
     },
 
 
     {
-        "SLTI", // MIPS instruction name
-        I_TYPE,
-        "001010",
-        NULL,
-        { RT, RS, IMM, EMPTY}
-
-
+        "SLTI",                 // MIPS instruction name
+        I_TYPE,                 // Instruction Type
+        "001010",               // Opcode
+        NULL,                   // Funct code
+        { RT, RS, IMM, EMPTY}   // Registers
     }
-};
-
-        { RS, IMM, RT, EMPTY }
-    }
-    
-    // TODO: Implement MULT, ORI, and SLTI instructions once bugs are fixed
 };
